fb19d317d8d52c0c1e25b3ec9bd4341c53e57cc1
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 09:29:01 -0330

    Replaced routes with getPages

--- lib/utilities/routes.dart
+++ lib/utilities/routes.dart
@@ -3,16 +3,26 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:forecast_v3/models/models.dart';
 import 'package:forecast_v3/pages/pages.dart';
 import 'package:forecast_v3/redux/actions.dart';
+import 'package:get/get.dart';
 
 class AppRoutes {
   static const String initialRoute = DashboardPage.routeName;
 
-  static final Map<String, Widget Function(BuildContext)> routes = {
-    '/': (final BuildContext ctx) => DashboardPage(
-          onInit: () {
-            StoreProvider.of<AppState>(ctx).dispatch(grabUserLocationAction);
-          },
-        ),
-    SettingsPage.routeName: (final BuildContext ctx) => const SettingsPage(),
-  };
+  // static final Map<String, Widget Function(BuildContext)> routes = {
+  //   '/': (final BuildContext ctx) => DashboardPage(
+  //         onInit: () {
+  //           StoreProvider.of<AppState>(ctx).dispatch(grabUserLocationAction);
+  //         },
+  //       ),
+  //   SettingsPage.routeName: (final BuildContext ctx) => const SettingsPage(),
+  // };
+
+  static final List<GetPage<dynamic>> getPages = <GetPage<dynamic>>[
+    GetPage(
+      name: DashboardPage.routeName,
+      page: DashboardPage.new,
+    ),
+  ];
 }
+
+
