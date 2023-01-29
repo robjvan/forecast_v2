2615ba2974dcc8e756d866368dc86874885ed401
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 09:28:22 -0330

    Added StoreConnector and onInit method

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:forecast_v3/models/models.dart';
+import 'package:forecast_v3/pages/dashboard/dashboard_view_model.dart';
+import 'package:forecast_v3/redux/actions.dart';
+import 'package:redux/redux.dart';
 
 class DashboardPage extends StatefulWidget {
-  final Function onInit;
-  const DashboardPage({required this.onInit, super.key});
+  // final Function onInit;
+  // const DashboardPage({required this.onInit, super.key});
+  const DashboardPage({super.key});
 
   @override
   State<DashboardPage> createState() => _DashboardPageState();
@@ -10,14 +16,24 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  void onInit(final Store<AppState> store) {
+    store.dispatch(grabUserLocationAction);
+  }
+
   @override
   void initState() {
     super.initState();
-    widget.onInit();
+    // widget.onInit();
   }
 
   @override
   Widget build(final BuildContext context) {
-    return const Placeholder();
+    return StoreConnector<AppState, DashboardPageViewModel>(
+      onInit: onInit,
+      converter: DashboardPageViewModel.create,
+      builder: (final BuildContext context, final DashboardPageViewModel vm) {
+        return Container();
+      },
+    );
   }
 }
