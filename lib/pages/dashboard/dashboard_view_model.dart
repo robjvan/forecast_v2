656e537c79b0a8f2a272f2b3d25e81a21034f81e
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 01:35:13 -0330

    Added ViewModel skeleton

--- lib/pages/dashboard/dashboard_view_model.dart
+++ lib/pages/dashboard/dashboard_view_model.dart
@@ -1,1 +1,24 @@
-class DashboardPageViewModel {}
+import 'package:flutter/material.dart';
+import 'package:forecast_v3/models/models.dart';
+import 'package:redux/redux.dart';
+
+@immutable
+class DashboardPageViewModel {
+  final double currentTempC;
+  final double currentTempF;
+  final double currentTempK;
+
+  const DashboardPageViewModel({
+    required this.currentTempC,
+    required this.currentTempF,
+    required this.currentTempK,
+  });
+
+  factory DashboardPageViewModel.create(final Store<AppState> store) {
+    return const DashboardPageViewModel(
+      currentTempC: -999,
+      currentTempF: -999,
+      currentTempK: -999,
+    );
+  }
+}
--- lib/pages/settings/settings_view_model.dart
+++ lib/pages/settings/settings_view_model.dart
@@ -1,1 +1,18 @@
-class SettingsPageViewModel {}
+import 'package:flutter/material.dart';
+import 'package:forecast_v3/models/models.dart';
+import 'package:redux/redux.dart';
+
+@immutable
+class SettingsPageViewModel {
+  final bool useDarkMode;
+
+  const SettingsPageViewModel({
+    required this.useDarkMode,
+  });
+
+  factory SettingsPageViewModel.create(final Store<AppState> store) {
+    return SettingsPageViewModel(
+      useDarkMode: store.state.userSettings.useDarkMode,
+    );
+  }
+}
