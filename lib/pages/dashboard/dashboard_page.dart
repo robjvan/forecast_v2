fa29221284e9607f2599d4903d24070284b72e71
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 14:49:16 -0330

    Added placeholder widgets for testing

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -6,8 +6,6 @@ import 'package:forecast_v3/redux/actions.dart';
 import 'package:redux/redux.dart';
 
 class DashboardPage extends StatefulWidget {
-  // final Function onInit;
-  // const DashboardPage({required this.onInit, super.key});
   const DashboardPage({super.key});
 
   @override
@@ -16,23 +14,61 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  void onInit(final Store<AppState> store) {
-    store.dispatch(grabUserLocationAction);
+  Future<String> checkForLocalData() async {
+    bool foundLocalData = false;
+
+    // Check for local data stored using Getx
+    // foundLocalData = await stuff();
+
+    if (foundLocalData) {
+      // if local data found, simply return OK
+      return 'ok';
+    }
+
+    // if no local data found, return error
+    return 'error';
   }
 
-  @override
-  void initState() {
-    super.initState();
-    // widget.onInit();
+  Future<void> onInit(final Store<AppState> store) async {
+    if (store.state.loadingState != LoadingState.loading) {
+      store.dispatch(const SetLoadingStateAction(LoadingState.loading));
+    }
+
+    // TODO: Check Get.storage for saved location list
+    final String dataCheckStatus = await checkForLocalData();
+    if (dataCheckStatus == 'ok') {
+      // If there IS a list, fire actions to:
+      // load userSettings and apply
+      // load loation list and fetch the weather for each one
+    } else {
+      // If there IS NOT a list, grab user location and weather data
+      await store.dispatch(grabUserLocationAction);
+      await store.dispatch(fetchWeatherDataAction(0));
+      store.dispatch(const SetLoadingStateAction(LoadingState.done));
+    }
   }
 
   @override
   Widget build(final BuildContext context) {
     return StoreConnector<AppState, DashboardPageViewModel>(
+      distinct: true,
       onInit: onInit,
       converter: DashboardPageViewModel.create,
       builder: (final BuildContext context, final DashboardPageViewModel vm) {
-        return Container();
+        return Scaffold(
+          body: Center(
+            child: vm.loadingState == LoadingState.loading
+                ? const CircularProgressIndicator()
+                : Center(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(vm.name),
+                      Text(vm.currentTempC.toString())
+                    ],
+                  )),
+          ),
+        );
       },
     );
   }
--- lib/pages/dashboard/dashboard_view_model.dart
+++ lib/pages/dashboard/dashboard_view_model.dart
@@ -7,18 +7,36 @@ class DashboardPageViewModel {
   final double currentTempC;
   final double currentTempF;
   final double currentTempK;
+  final LoadingState loadingState;
+  final String name;
 
   const DashboardPageViewModel({
     required this.currentTempC,
     required this.currentTempF,
     required this.currentTempK,
+    required this.name,
+    required this.loadingState,
   });
 
   factory DashboardPageViewModel.create(final Store<AppState> store) {
-    return const DashboardPageViewModel(
-      currentTempC: -999,
-      currentTempF: -999,
+    int getCurrentLocationIndex() {
+      return store.state.currentLocationIndex;
+    }
+
+    return DashboardPageViewModel(
+      currentTempC: store.state.weatherData.isNotEmpty
+          ? store.state.weatherData[getCurrentLocationIndex()].currentConditions
+              .temp_c!
+          : -999,
+      currentTempF: store.state.weatherData.isNotEmpty
+          ? store.state.weatherData[getCurrentLocationIndex()].currentConditions
+              .temp_f!
+          : -999,
       currentTempK: -999,
+      name: store.state.locations.isNotEmpty
+          ? store.state.locations[getCurrentLocationIndex()].name!
+          : '',
+      loadingState: store.state.loadingState,
     );
   }
 }
