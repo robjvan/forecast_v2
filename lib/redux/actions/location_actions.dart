0c32b9ca4a5bfb4e31e5f10add2db0c96406a2b6
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 19:48:51 -0330

    Removed unused imports

--- lib/redux/actions/location_actions.dart
+++ lib/redux/actions/location_actions.dart
@@ -4,13 +4,9 @@ import 'dart:developer';
 
 import 'package:forecast_v3/models/models.dart';
 import 'package:forecast_v3/providers/geolocation_provider.dart';
-import 'package:forecast_v3/utilities/utilities.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:get/get.dart';
 import 'package:redux/redux.dart';
-import 'package:redux_logging/redux_logging.dart';
 import 'package:redux_thunk/redux_thunk.dart';
-import 'package:google_geocoding_api/google_geocoding_api.dart';
 
 class UpdateLocationListAction {
   final List<Location> locations;
