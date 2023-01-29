44d37f9d6bbfeb2984e7fb2f58870bc423cc086c
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 09:27:41 -0330

    Added routes and translations

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:forecast_v3/models/models.dart';
 import 'package:forecast_v3/redux/reducers.dart';
 import 'package:forecast_v3/utilities/utilities.dart';
+import 'package:get/get.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_logging/redux_logging.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final Store<AppState> store = Store<AppState>(
     appReducer,
     initialState: AppState.initial(),
@@ -32,11 +35,12 @@ class MyApp extends StatelessWidget {
   Widget build(final BuildContext context) {
     return StoreProvider<AppState>(
       store: store,
-      child: MaterialApp(
+      child: GetMaterialApp(
+        translations: AppTranslations(),
+        getPages: AppRoutes.getPages,
         title: 'Forecast',
         theme: AppTheme.themeData,
         initialRoute: AppRoutes.initialRoute,
-        routes: AppRoutes.routes,
       ),
     );
   }
