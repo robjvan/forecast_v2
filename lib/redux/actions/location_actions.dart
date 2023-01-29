2ebb024e1906c4509c108b0fa176f3dc1768cf77
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 01:35:53 -0330

    Formatted code for clarity

--- lib/redux/actions/location_actions.dart
+++ lib/redux/actions/location_actions.dart
@@ -1,6 +1,13 @@
+// These actions are used to interact with Location state
+
 import 'package:forecast_v3/models/models.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
+
+class UpdateLocationListAction {
+  final List<Location> newList;
+  const UpdateLocationListAction(this.newList);
+}
 
 ThunkAction<AppState> grabUserLocationAction =
     (final Store<AppState> store) async {
@@ -8,8 +15,3 @@ ThunkAction<AppState> grabUserLocationAction =
   // dispatch send coords to locationsList reducer action
   // dispatch send coords to fetch weather data action
 };
-
-class UpdateLocationListAction {
-  final List<Location> newList;
-  const UpdateLocationListAction(this.newList);
-}
