7791316bc6156b742555fa01fd21c0a4394f236c
Rob Vandelinder <robjvan@gmail.com>
2023-01-29 09:28:48 -0330

    Built basic AppIcons class

---
+++ lib/utilities/icons.dart
@@ -0,0 +1,7 @@
+class AppIcons {
+  // static const String barcode = 'assets/icons/barcode.svg';
+}
+
+class WeatherIcons {
+  // static const String barcode = 'assets/weather-icons/barcode.svg';
+}
