9ee437130c5f35d629a70ac296fbc3e556e05ea4
Rob Vandelinder <robjvan@gmail.com>
2023-01-30 01:02:53 -0330

    Added GooglePlaceAutoCompleteTextField widget

--- lib/widgets/settings_drawer/widgets/location_selector.dart
+++ lib/widgets/settings_drawer/widgets/location_selector.dart
@@ -1,8 +1,14 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:forecast_v3/models/models.dart';
+import 'package:forecast_v3/redux/actions.dart';
 import 'package:forecast_v3/utilities/utilities.dart';
 import 'package:forecast_v3/widgets/settings_drawer/settings_drawer_view_model.dart';
 import 'package:get/get.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:google_places_flutter/model/prediction.dart';
 
 class LocationSelector extends StatefulWidget {
   final SettingsDrawerViewModel viewModel;
@@ -29,20 +35,30 @@ class _LocationSelectorState extends State<LocationSelector> {
         borderRadius: BorderRadius.circular(8),
         border: Border.all(color: widget.viewModel.textColor),
       ),
-      child: TextField(
-        style: TextStyle(
+      child: GooglePlaceAutoCompleteTextField(
+        googleAPIKey: AppKeys.googleApiKey,
+        textStyle: TextStyle(
           fontSize: 16,
           overflow: TextOverflow.visible,
           color: widget.viewModel.textColor,
         ),
-        controller: _locationController,
-        onChanged: (final String locToSearch) async {
-          // TODO(Rob): Add Google Places autocomplete logic here to show suggestions as the user types
+        textEditingController: _locationController,
+        countries: const <String>['ca', 'us', 'uk'],
+        isLatLngRequired: true,
+        getPlaceDetailWithLatLng: (final Prediction prediction) {
+          // this method will return latlng with place detail
+          final Location newLocation = Location(
+            latitude: double.parse(prediction.lat!),
+            longitude: double.parse(prediction.lng!),
+            name: prediction.description!.split(', ')[0],
+            region: prediction.description!.split(', ')[1],
+          );
+          widget.viewModel.dispatch(addLocationToListAction(newLocation));
         },
-        // onEditingComplete: () {
-        //   //
-        // },
-        decoration: InputDecoration(
+        itmClick: (final Prediction prediction) {
+          _locationController.clear();
+        },
+        inputDecoration: InputDecoration(
           icon: Container(
             margin: const EdgeInsets.only(left: 16),
             width: 20,
