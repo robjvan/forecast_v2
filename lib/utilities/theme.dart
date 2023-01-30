a0cdf366b0d130210ca5e428d8231f26a4e8915e
Rob Vandelinder <robjvan@gmail.com>
2023-01-30 01:06:23 -0330

    Added more styles and colors

--- lib/utilities/theme.dart
+++ lib/utilities/theme.dart
@@ -37,15 +37,39 @@ class AppTheme {
   );
 }
 
-class AppStyles {    
-  // static TextStyle productDetailNameStyle = const TextStyle(
-  //   fontSize: 28.0,
-  //   fontStyle: FontStyle.italic,
-  // );
+class AppStyles {
+  static const TextStyle drawerHeaderTextStyle = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Raleway',
+  );
+  static const TextStyle settingsButtonStyle = TextStyle(
+    fontSize: 18,
+    fontFamily: 'CarroisSC',
+  );
+  static const TextStyle cardHeaderStyle = TextStyle(
+    fontSize: 14,
+    // fontWeight: FontWeight.bold,
+    fontFamily: 'Montserrat',
+  );
+  static const TextStyle cardBodyStyle = TextStyle(
+    fontSize: 18,
+  );
 }
 
 class AppColors {
-  //
+  static const Color textColorLightMode = darkGrey;
+  static const Color textColorDarkMode = white;
+  static const Color bgColorLightMode = white;
+  static const Color bgColorDarkMode = darkGrey;
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color green = Color(0xFF00FF00);
+  static const Color blue = Color(0xFF0000FF);
+  static const Color red = Color(0xFFFF0000);
+  static const Color grey = Color(0xFF808080);
+  static const Color lightGrey = Color(0xFFDDDDDD);
+  static const Color darkGrey = Color(0xFF202020);
 }
 
 class AppFonts {
