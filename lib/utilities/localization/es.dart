d66e75bece6cc41d34a0d593ee44a65241e52551
Rob Vandelinder <robjvan@gmail.com>
2023-01-30 01:39:38 -0330

    Added localization strings

--- lib/utilities/localization/es.dart
+++ lib/utilities/localization/es.dart
@@ -1,3 +1,8 @@
 const Map<String, String> spanishStrings = {
   'app_title': 'Forecast',
+  'settings.settings_header': 'Ajustes',
+  'settings.select_language': 'Seleccione el Idioma',
+  'settings.en': 'English',
+  'settings.fr': 'Français',
+  'settings.es': 'Español',
 };
--- lib/utilities/localization/fr.dart
+++ lib/utilities/localization/fr.dart
@@ -1,3 +1,8 @@
 const Map<String, String> frenchStrings = {
   'app_title': 'Forecast',
+  'settings.settings_header': 'Paramètres',
+  'settings.select_language': 'Choisir la Langue',
+  'settings.en': 'English',
+  'settings.fr': 'Français',
+  'settings.es': 'Español',
 };
