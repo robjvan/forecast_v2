c79538b68d0d5a1fd91264ddb0d0a9fd8af08b07
Rob Vandelinder <robjvan@gmail.com>
2023-03-31 20:37:04 -0230

    Removed buildContent function

--- lib/pages/dashboard/widgets/cards/current_conditions_card.dart
+++ lib/pages/dashboard/widgets/cards/current_conditions_card.dart
@@ -1,13 +1,17 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:forecast_v3/pages/dashboard/widgets/cards/current_conditions_view_model.dart';
+import 'package:forecast_v3/pages/dashboard/widgets/dialogs/weather_alert_dialog.dart';
 import 'package:forecast_v3/redux/app_state.dart';
-import 'package:forecast_v3/pages/dashboard/widgets/dashboard_widgets_view_model.dart';
 import 'package:forecast_v3/utilities/utilities.dart';
 import 'package:get/get.dart';
-import 'package:weather_icons/weather_icons.dart';
 
-class CurrentConditionsBox extends StatelessWidget {
-  const CurrentConditionsBox({super.key});
+@immutable
+class CurrentConditionsCard extends StatelessWidget {
+  const CurrentConditionsCard({super.key});
+
+  /// Build alert button if there are active weather alerts
   Widget buildAlertButton() {
     return Positioned(
       right: 8,
@@ -18,21 +22,37 @@ class CurrentConditionsBox extends StatelessWidget {
           color: Colors.red,
           size: 40.0,
         ),
-        onPressed: () {},
+        onPressed: () => Get.dialog(const WeatherAlertDialog()),
       ),
     );
   }
 
-  Widget buildCityName(final DashboardWidgetsViewModel vm) {
-    return Text(
-      vm.weatherDataList[vm.activeLocationIndex].location.name.toString(),
-      style: AppStyles.locationNameTextStyle.copyWith(
-        color: vm.textColor,
-      ),
+  /// Build city name widget
+  Widget buildCityName(final CurrentConditionsViewModel vm) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          width: vm.activeWeatherAlert
+              ? (Get.width - 128) * 0.75
+              : (Get.width - 128),
+          child: Text(
+            vm.weatherDataList[vm.activeLocationIndex].location.name.toString(),
+            style: AppStyles.locationNameTextStyle.copyWith(
+              color: vm.textColor,
+              overflow: TextOverflow.ellipsis,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        // vm.activeWeatherAlert ? buildAlertButton() : Container(),
+      ],
     );
   }
 
-  Widget buildCurrentTemp(final DashboardWidgetsViewModel vm) {
+  /// Build current temp widget
+  Widget buildCurrentTemp(final CurrentConditionsViewModel vm) {
     return Text(
       vm.currentTemp,
       style: AppStyles.currentTempTextStyle.copyWith(
@@ -41,31 +61,34 @@ class CurrentConditionsBox extends StatelessWidget {
     );
   }
 
-  Widget buildWeatherIcon(final DashboardWidgetsViewModel vm) {
-    return BoxedIcon(
-      vm.weatherIcon(
-        vm.weatherDataList[vm.activeLocationIndex].currentConditions.condition!
-            .code!,
+  /// Build current weather icon widget
+  Widget buildWeatherIcon(final CurrentConditionsViewModel vm) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      // padding: const EdgeInsets.only(bottom: 0.0),
+      child: Transform.scale(
+        scale: 1.25,
+        child: CachedNetworkImage(imageUrl: 'http:${vm.conditionsIconUrl}'),
       ),
-      size: 100,
-      color: vm.textColor,
     );
   }
 
-  Widget buildConditionsString(final DashboardWidgetsViewModel vm) {
+  /// Build current weather conditions widget
+  Widget buildConditionsString(final CurrentConditionsViewModel vm) {
     return Text(
-      vm.currentConditions,
+      vm.currentConditions.tr,
       style: AppStyles.currentConditionsStyle.copyWith(
         color: vm.textColor,
       ),
     );
   }
 
-  Widget buildFeelsLikeString(final DashboardWidgetsViewModel vm) {
+  /// Build current "feels like" widget
+  Widget buildFeelsLikeString(final CurrentConditionsViewModel vm) {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
       child: Text(
-        'Feels like ${vm.feelsLikeTemp}', // TODO(Rob): Add i18n strings
+        'feelslike'.trParams(<String, String>{'temp': vm.feelsLikeTemp}),
         style: AppStyles.currentFeelsLikeTextStyle.copyWith(
           color: vm.textColor,
         ),
@@ -73,33 +96,12 @@ class CurrentConditionsBox extends StatelessWidget {
     );
   }
 
-  Widget buildContent(final DashboardWidgetsViewModel vm) {
-    return Stack(
-      children: <Widget>[
-        buildAlertButton(),
-        Container(
-          width: Get.width,
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Column(
-            children: <Widget>[
-              buildCityName(vm),
-              buildCurrentTemp(vm),
-              buildWeatherIcon(vm),
-              buildConditionsString(vm),
-              buildFeelsLikeString(vm),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(final BuildContext context) {
-    return StoreConnector<AppState, DashboardWidgetsViewModel>(
+    return StoreConnector<AppState, CurrentConditionsViewModel>(
       distinct: true,
-      converter: DashboardWidgetsViewModel.create,
-      builder: (final _, final DashboardWidgetsViewModel vm) {
+      converter: CurrentConditionsViewModel.create,
+      builder: (final _, final CurrentConditionsViewModel vm) {
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 64.0),
           child: DecoratedBox(
@@ -107,7 +109,23 @@ class CurrentConditionsBox extends StatelessWidget {
               borderRadius: BorderRadius.circular(8.0),
               color: vm.cardColor.withOpacity(0.5),
             ),
-            child: buildContent(vm),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  width: Get.width,
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Column(
+                    children: <Widget>[
+                      buildCityName(vm),
+                      buildCurrentTemp(vm),
+                      buildWeatherIcon(vm),
+                      buildConditionsString(vm),
+                      buildFeelsLikeString(vm),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
