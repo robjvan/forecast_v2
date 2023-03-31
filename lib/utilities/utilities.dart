eee6b9f60cdde523ccfaac588ac9cd05ad8974a7
Rob Vandelinder <robjvan@gmail.com>
2023-03-31 20:30:53 -0230

    Added double.toKelvin extension

--- lib/utilities/utilities.dart
+++ lib/utilities/utilities.dart
@@ -6,3 +6,9 @@ export 'theme.dart';
 export 'translations.dart';
 export 'validators.dart';
 export 'weather_icon_parser.dart';
+
+// double toKelvin(final double input) => input + 273.15;
+
+extension ToKelvin on double {
+  double toKelvin() => this + 273.15;
+}
