fd5a004e840cbf780c3e716c84e65f45f2db5b11
Rob Vandelinder <robjvan@gmail.com>
2023-03-31 20:32:42 -0230

    Added language code as optional param

--- lib/providers/weather_data_provider.dart
+++ lib/providers/weather_data_provider.dart
@@ -7,16 +7,16 @@ import 'package:http/http.dart';
 class WeatherDataProvider {
   /// Grab weather data from the API
   static Future<APIResponse<dynamic>> fetchWeatherData(
-      // final String locationString,
+    final String? lang,
       {
     required final String lat,
     required final String long,
   }) async {
     final Response response =
-        await WeatherAPIProvider().getWeatherDataFromApi(lat, long);
-    // await WeatherAPIProvider().getWeatherDataFromApi(locationString);
-    // Return error code is status is not 200 or 201
-    if (response.statusCode != 200) {
+        await WeatherAPIProvider().getWeatherDataFromApi(lat, long, lang);
+
+    // Return error code if status is not 200 or 201
+    if (response.statusCode != 200 && response.statusCode != 201) {
       return APIResponse<dynamic>(
         isOk: false,
         result: null,
